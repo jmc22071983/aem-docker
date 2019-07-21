75d702678f0b80342cf8537643c3372e57434419
javier <javier.monje.cerrato@gmail.com>
2019-07-21 20:04:42 +0200

    mod sh

--- setup-stack.sh
+++ setup-stack.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-docker-machine rm -f default
-echo "Deleted docker machine default";
-docker-machine create -d virtualbox  --virtualbox-disk-size "30000" --virtualbox-memory=8192 default
+echo "THE SCRIPT WILL CREATE A NEW DOCKER MACHINE";
+echo -n "Press [ENTER] to continue,...: "
+read var_name
+docker-machine create -d virtualbox  --virtualbox-disk-size "30000" --virtualbox-memory=8192 aem-node1
 echo "Created docker machine default with 30Gb Disk and 8Gb RAM";
-docker swarm init --advertise-addr $(docker-machine ip default):2377
+docker swarm init --advertise-addr $(docker-machine ip aem-node1):2377
