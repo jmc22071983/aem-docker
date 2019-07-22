ed0c9de18f6ba818c78bb17b67080d099ff80531
30044840 <javier.monje@ext.leroymerlin.es>
2019-07-22 10:39:00 +0200

    mod

--- setup-stack-aem632.sh
+++ setup-stack-aem632.sh
@@ -2,7 +2,7 @@
 echo "******************************************************************************************** "
 echo "*********** AEM 6.3.2 SWARM STACK: AUTHOR, PUBLISH AND DISPATCHER INSTANCES **************** "
 echo "******************************************************************************************** "
-eval $(docker-machine env aem1)
+eval $(docker-machine env default)
 echo "Downloading docker-compose.yml from github ...";
 curl -o docker-compose.yml  https://raw.githubusercontent.com/jmc22071983/aem-docker/master/docker-compose.yml
 echo "Deploying stack swarm aem ...";
