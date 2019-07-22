a50d0fcbc0b20a9c52b5bf405ec1060f79248bcf
30044840 <javier.monje@ext.leroymerlin.es>
2019-07-22 10:36:31 +0200

    mod

--- setup-stack.sh
+++ setup-stack.sh
@@ -4,6 +4,11 @@ echo -n "Press [ENTER] to continue,...: "
 read var_name
 docker-machine create -d virtualbox  --virtualbox-disk-size "30000" --virtualbox-memory=8192 aem1
 echo "Created docker machine aem1 with 30Gb Disk and 8Gb RAM";
+echo "Init Docker Swarm: Dafault machine will be the MANAGER: "
+eval $(docker-machine env default)
+docker swarm init --advertise-addr $(docker-machine ip default):2377
+TOKEN=$(docker swarm join-token -q worker)
+echo "Docker machine aem1 will be the worker"
 eval $(docker-machine env aem1)
-docker swarm init --advertise-addr $(docker-machine ip aem1):2377
+docker swarm join --token $TOKEN $(docker-machine ip default):2377
 echo "swarn finished";
