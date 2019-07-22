8b6767b462710559fd44d03de7c3294a82badbce
30044840 <javier.monje@ext.leroymerlin.es>
2019-07-22 09:23:11 +0200

    mod

--- setup-stack-aem632.sh
+++ setup-stack-aem632.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-echo "********************************************************************************** "
-echo "*********** AEM 6.3.2 STACK DEPLOY WITH AUTHOR, PUBLISH AND DISPATCHER *********** "
-echo "********************************************************************************** "
-eval $(docker-machine env aem)
+echo "******************************************************************************************** "
+echo "*********** AEM 6.3.2 SWARM STACK: AUTHOR, PUBLISH AND DISPATCHER INSTANCES **************** "
+echo "******************************************************************************************** "
+eval $(docker-machine env aem1)
 echo "Downloading docker-compose.yml from github ...";
 curl -o docker-compose.yml  https://raw.githubusercontent.com/jmc22071983/aem-docker/master/docker-compose.yml
 echo "Deploying stack swarm aem ...";
 docker stack deploy -c docker-compose.yml aem632-stack
-echo "Wait..., have a coffee until all services are running"
+echo "Wait..., have a coffee until all services be running"
--- setup-stack.sh
+++ setup-stack.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-echo "THE SCRIPT WILL CREATE A NEW DOCKER MACHINE";
+echo "THE SCRIPT WILL CREATE A NEW DOCKER MACHINE FOR AEM INSTANCES";
 echo -n "Press [ENTER] to continue,...: "
 read var_name
-docker-machine create -d virtualbox  --virtualbox-disk-size "30000" --virtualbox-memory=8192 aem
-echo "Created docker machine aem-node1 with 30Gb Disk and 8Gb RAM";
-eval $(docker-machine env aem)
-docker swarm init --advertise-addr $(docker-machine ip aem):2377
+docker-machine create -d virtualbox  --virtualbox-disk-size "30000" --virtualbox-memory=8192 aem1
+echo "Created docker machine aem1 with 30Gb Disk and 8Gb RAM";
+eval $(docker-machine env aem1)
+docker swarm init --advertise-addr $(docker-machine ip aem1):2377
 echo "swarn finished";
