45bbc5d7e3e6e1ebf151a7cc2383865e8f2493aa
javier <javier.monje.cerrato@gmail.com>
2019-07-24 00:02:00 +0200

    añadidos compose

--- setup-stack-aem632-complete.sh
+++ setup-stack-aem632-complete.sh
@@ -7,4 +7,4 @@ echo "Downloading docker-compose.yml from github ...";
 curl -o docker-compose.yml  https://raw.githubusercontent.com/jmc22071983/aem-docker/master/docker-compose.yml
 echo "Deploying stack swarm aem ...";
 docker stack deploy -c docker-compose.yml aem632-stack
-echo "Wait..., have a coffee until all services be running"
+echo "Wait..., have a coffee until all services be running, mejor vete a dar un paseo..."
