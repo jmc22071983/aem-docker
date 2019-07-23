88431ba81cd36659112345abaa0471e4a6e8cdb4
javier <javier.monje.cerrato@gmail.com>
2019-07-23 23:20:06 +0200

    mod

--- publish-dispatcher.sh
+++ publish-dispatcher.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "RUNNING AEM-Publish Instance-SP:3.2.2 ..."
-docker run -d -e RUNMODE=$1 --rm -v aem632-publish:/crx-quickstart/repository -v /aem632-publish/logs:/crx-quickstart/logs -p 4503:4503 -p 8001:8001 jmc22071983/aem632-publish:0.0.4-SNAPSHOT 
+docker run -d -e RUNMODE=$1 --rm -v aem632-publish:/crx-quickstart/repository -v /aem632-publish/logs:/crx-quickstart/logs -p 4503:4503 -p 8001:8001 jmc22071983/aem632-publish:0.0.5-SNAPSHOT 
 eval $(docker-machine env default)
 echo "RUNNING Dispatcher..."
 DEFAULTIP=$(docker-machine ip default)
--- setup-stack.sh
+++ setup-stack.sh
@@ -4,7 +4,7 @@ echo -n "Press [ENTER] to continue,...: "
 read var_name
 docker-machine rm -f default
 docker-machine create -d virtualbox  --virtualbox-disk-size "35000" --virtualbox-memory=8192 default
-echo "Created docker machine aem1 with 30Gb Disk and 8Gb RAM";
+echo "Created docker machine default with 30Gb Disk and 8Gb RAM";
 echo "Init Docker Swarm: Dafault machine will be the MANAGER: "
 eval $(docker-machine env default)
 docker swarm init --advertise-addr $(docker-machine ip default):2377
