eca9a0216626e9cdd19af6dbd3d9c764e07ab02c
javier <javier.monje.cerrato@gmail.com>
2019-07-24 00:07:36 +0200

    añadidos compose

--- setup-stack-aem632-complete.sh
+++ setup-stack-aem632-complete.sh
@@ -4,7 +4,7 @@ echo "*********** AEM 6.3.2 SWARM STACK: AUTHOR, PUBLISH AND DISPATCHER INSTANCE
 echo "******************************************************************************************** "
 eval $(docker-machine env default)
 echo "Downloading docker-compose.yml from github ...";
-curl -o docker-compose.yml  https://raw.githubusercontent.com/jmc22071983/aem-docker/master/docker-compose.yml
+curl -o author-publish-dispatcher.yml  https://raw.githubusercontent.com/jmc22071983/aem-docker/master/aem6.3.2-author-publish-dispatcher.yml
 echo "Deploying stack swarm aem ...";
-docker stack deploy -c docker-compose.yml aem632-stack
+docker stack deploy -c aem6.3.2-author-publish-dispatcher.yml aem632-stack
 echo "Wait..., have a coffee until all services be running, mejor vete a dar un paseo..."
