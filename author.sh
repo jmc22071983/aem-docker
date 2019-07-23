fd0fa3481448480c9cfe74ecf11ba355d774b0fd
javier <javier.monje.cerrato@gmail.com>
2019-07-24 00:49:50 +0200

    mod

--- author.sh
+++ author.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-echo "RUNNING AEM-Author Instance-SP:3.2.2";
-docker run -e RUNMODE=$1 --rm -v aem632-author:/crx-quickstart/repository -v /aem632-author/logs:/crx-quickstart/logs  -p 4502:4502 -p 8000:8000 jmc22071983/aem632-author:0.0.9-SNAPSHOT
+echo "******************************************************************************************** "
+echo "*********** AEM 6.3.2 SWARM STACK: AUTHOR, PUBLISH AND DISPATCHER INSTANCES **************** "
+echo "******************************************************************************************** "
+eval $(docker-machine env default)
+echo "Downloading docker-compose.yml from github ...";
+curl -o author.yml  https://raw.githubusercontent.com/jmc22071983/aem-docker/master/aem6.3.2-author.yml
+echo "Deploying stack swarm aem ...";
+env RUNMODE=$1 docker stack deploy -c author.yml aem632-stack
+echo "Wait..., have a coffee until all services be running, mejor vete a dar un paseo..."
