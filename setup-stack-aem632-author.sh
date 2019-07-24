9c8add8f438534344f765ad71dfeffbb7b2098bf
30044840 <javier.monje@ext.leroymerlin.es>
2019-07-24 09:11:28 +0200

    mod env runmode

--- setup-stack-aem632-author.sh
+++ setup-stack-aem632-author.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "******************************************************************************************** "
-echo "*********** AEM 6.3.2 SWARM STACK: AUTHOR, PUBLISH AND DISPATCHER INSTANCES **************** "
+echo "*********** AEM 6.3.2 SWARM STACK: AUTHOR INSTANCE **************** "
 echo "******************************************************************************************** "
 eval $(docker-machine env default)
 echo "Downloading docker-compose.yml from github ...";
--- setup-stack-aem632-publish-dispatcher.sh
+++ setup-stack-aem632-publish-dispatcher.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo "******************************************************************************************** "
-echo "*********** AEM 6.3.2 SWARM STACK: AUTHOR, PUBLISH AND DISPATCHER INSTANCES **************** "
+echo "*********** AEM 6.3.2 SWARM STACK: PUBLISH AND DISPATCHER INSTANCES **************** "
 echo "******************************************************************************************** "
 eval $(docker-machine env default)
 echo "Downloading docker-compose.yml from github ...";
 curl -o publish-dispatcher.yml  https://raw.githubusercontent.com/jmc22071983/aem-docker/master/aem6.3.2-publish-dispatcher.yml
 echo "Deploying stack swarm aem ...";
-docker stack deploy -c publish-dispatcher.yml aem632-stack
+env RUNMODE=$1 docker stack deploy -c publish-dispatcher.yml aem632-stack
 echo "Wait..., have a coffee until all services be running, mejor vete a dar un paseo..."
