a17a23e711752eb5e940ce71fd3520d0870c2d20
jmc22071983 <javier.monje.cerrato@gmail.com>
2019-09-14 19:37:46 +0200

    mod

---
+++ setup-stack-aem6.5-publish-dispatcher.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "******************************************************************************************** "
+echo "*********** AEM 6.3.2 SWARM STACK: AUTHOR, PUBLISH AND DISPATCHER INSTANCES **************** "
+echo "******************************************************************************************** "
+env docker swarm init
+echo "Downloading docker-compose.yml from github ...";
+curl -o aem6.5-complete.yml  https://raw.githubusercontent.com/jmc22071983/aem-docker/master/aem6.5-author-publish-dispatcher.yml
+read -p "Enter the AEM runmode: " run_mode
+echo "Deploying stack swarm aem ...";
+env RUNMODE=$run_mode docker-compose -f aem6.5-complete.yml up
