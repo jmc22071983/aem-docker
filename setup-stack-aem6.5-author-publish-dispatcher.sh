730be0c0a33a9a88fe098422829bc8b515e3145b
jmc22071983 <javier.monje.cerrato@gmail.com>
2019-09-15 10:51:56 +0200

    mod

--- setup-stack-aem6.5-author-publish-dispatcher.sh
+++ setup-stack-aem6.5-author-publish-dispatcher.sh
@@ -10,9 +10,10 @@ echo "Creating the node manager SWARM...";
 env docker swarm init
 echo "Downloading compose: aem65-complete.yml from github ...";
 curl -o aem65-complete.yml  https://raw.githubusercontent.com/jmc22071983/aem-docker/master/aem6.5-author-publish-dispatcher.yml
-read -p "Enter the run mode you want to boot the AEM instances (For example: author, publish, stg, local...): " run_mode
+read -p "Enter the run mode you want to boot the AEM author instance (For example: author, local, ...): " run_mode_auth
+read -p "Enter the run mode you want to boot the AEM publish instance (For example: publish, stg, ...): " run_mode_pub
 echo "Deploying stack swarm AEM6.5 ...";
-env RUNMODE=$run_mode docker stack deploy -c aem65-complete.yml aem65-stack
+env RUNMODEAUT=$run_mode_auth RUNMODEPUB=$run_mode_pub docker stack deploy -c aem65-complete.yml aem65-stack
 echo "******************************************************************************************** ";
 echo "Tip 'docker service ls' to see the services";
 echo "Tip 'docker ps' to see the docker containers";
