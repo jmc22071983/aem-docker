3c0fcae9f1a75234223139ee968d238a9f9fde1e
jmc22071983 <javier.monje.cerrato@gmail.com>
2019-09-15 10:27:09 +0200

    mod

--- setup-stack-aem6.5-publish-dispatcher-san.sh
+++ setup-stack-aem6.5-publish-dispatcher-san.sh
@@ -10,5 +10,5 @@ env docker swarm init
 echo "Downloading compose: aem65-complete-san.yml from github ...";
 curl -o aem65-complete-san.yml  https://raw.githubusercontent.com/jmc22071983/aem-docker/master/aem6.5-author-publish-dispatcher-san.yml
 read -p "Enter the run mode you want to boot the AEM instances (For example: author, publish, stg, local...): " run_mode
-echo "Deploying stack swarm aem ...";
+echo "Deploying stack swarm AEM6.5 ...";
 env RUNMODE=$run_mode docker stack deploy -c aem65-complete-san.yml aem65-stack-san
